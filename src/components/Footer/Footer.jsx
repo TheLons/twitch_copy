ce85931333d964e89df08d8a5f6faaab1ac25454
boris <boristskhay@gmail.com>
2024-12-17 21:28:59 +0900

    fixed multiple bugs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
 import styles from './Footer.module.css'
+import FooterIcon from '../../assets/Icons/footer_icon.png'
 
 const Footer = () => {
     return (
         <div className={styles.footer}>
             <div className={styles.footer_desc}>
+                <img src={FooterIcon} alt="Footer icon" />
                 <p><span>Join the Twitch community!</span>Discover the best live streams anywhere.</p>
             </div>
             <button>
--- src/components/LeftLayer/AccountCard.jsx
+++ src/components/LeftLayer/AccountCard.jsx
@@ -10,15 +10,12 @@ const AccountCard = ({avatar, profile_name, game, number}) => {
             <div className={styles.container}>
                 <div className={styles.image_container}>
                     <img src={avatar} alt="avatar icon 1" />
+                    <div className={styles.description}>
+                        <p>{profile_name}</p>
+                        <p>{game}</p>
+                    </div>
                 </div>
-                <div className={styles.description}>
-                    <p>{profile_name}</p>
-                    <p>{game}</p>
-                </div>
-                <div className={styles.views}>
-                    <div className={styles.red_dot}></div>
-                    <div className={styles.number_of_views}>{number}</div>
-                </div>
+                <div className={styles.number_of_views}>{number}</div>
             </div>
         </>
     )
